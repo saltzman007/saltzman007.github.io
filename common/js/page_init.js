f84fda634ac2ff5ff1a45d33215ea8bae2e70b5f
The Byter <harrysaltzman@web.de>
2022-04-03 18:01:54 +0200

    =nicetry

--- common/js/page_init.js
+++ common/js/page_init.js
@@ -1,19 +1,19 @@
 
 (function(){
-	$('.sections > .section:not(.hide):not(.show)').addClass('hide').waypoint({
-		handler: function(direction) {
-			$(this.element).removeClass('hide').addClass('show');
-			this.destroy();
-		},
-		offset: '80%'
-	});
+	// $('.sections > .section:not(.hide):not(.show)').addClass('hide').waypoint({
+	// 	handler: function(direction) {
+	// 		$(this.element).removeClass('hide').addClass('show');
+	// 		this.destroy();
+	// 	},
+	// 	offset: '80%'
+	// });
 
-	$(function(){
-		Waypoint.refreshAll();
-		setTimeout(function(){
-			Waypoint.refreshAll();
-		},800)
-	})
+	// $(function(){
+	// 	Waypoint.refreshAll();
+	// 	setTimeout(function(){
+	// 		Waypoint.refreshAll();
+	// 	},800)
+	// })	
 
 
 })(jQuery);
