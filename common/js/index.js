45e9324d72e338a0a7462863f3edd6bd844bffe4
The Byter <harrysaltzman@web.de>
2022-11-10 15:01:00 +0100

    Index Orientationdependend Pics

--- common/js/index.js
+++ common/js/index.js
@@ -1,11 +1,16 @@
-(function($){
+(function ($) {
 
-	var $images = $('#images li');
+	var $images;
+	if (isPortrait())
+		$images = $('#imagesPortait li');
+	else
+		$images = $('#imagesLandscape li');
+		
 	var $title = $('#site-name');
 
 	var timer = subTimer = null;
 	var numImages = $images.length
-	var current = Math.floor(Math.random()*numImages);
+	var current = Math.floor(Math.random() * numImages);
 	var INTERVAL = 5000;
 
 
@@ -14,46 +19,50 @@
 		30, // 1 H
 		30, // 2 a
 		14, // 3 r
-		 2, // 4 r
-		 8, // 5 y
-		 4, // 6 
-		 30, // 7 S
+		2, // 4 r
+		8, // 5 y
+		4, // 6 
+		30, // 7 S
 		15, // 8 a
 		12, // 9 l
 		30, //10 t
-		 9, //11 z
+		9, //11 z
 		20, //12 m
 		50, //13 a
 		17, //14 n
-		10  
+		10
 	]
 
 	$title.html('');
 	//$.each(title.replace(/[\t\s]/g,'').split(''),function(i,c){
-	$.each(title.split(''),function(i,c){
+	$.each(title.split(''), function (i, c) {
 		//var $s = $('<span class="c'+(i+1)+'">'+c+'</span>');
-		var $s = $('<span class="c'+(i+1)+'">'+c+'</span>');
+		var $s = $('<span class="c' + (i + 1) + '">' + c + '</span>');
 		var d = typo_delay[i]
-		$s.css(Modernizr.prefixedCSS('transition-delay'), (d/30)*1000+'ms')
+		$s.css(Modernizr.prefixedCSS('transition-delay'), (d / 30) * 1000 + 'ms')
 		$title.append($s);
 	});
 
 
-	$images.each(function(){
+	$images.each(function () {
 		var $img = $(this).find('img');
 		$(this).css({
-			'background-image' : 'url('+$img.attr('src')+')'
+			'background-image': 'url(' + $img.attr('src') + ')'
 		});
-		$img.css('display','none');
+		$img.css('display', 'none');
 	});
 
-	function start(){
-		timer = setTimeout(function(){
+	function isPortrait() {
+		return window.innerHeight > window.innerWidth;
+	}
+
+	function start() {
+		timer = setTimeout(function () {
 			var old = current;
 			$images.eq(old).addClass('hide');
-			subTimer = setTimeout(function(){
+			subTimer = setTimeout(function () {
 				$images.eq(old).removeClass('current hide');
-			},2200)
+			}, 2200)
 			current = getRandomNext();
 			$images.eq(current).addClass('current');
 
@@ -62,57 +71,57 @@
 		}, INTERVAL);
 	}
 
-	function stop(){
+	function stop() {
 		clearTimeout(timer);
 		clearTimeout(subTimer);
 	}
 
-	function getRandomNext(){
-		var n = Math.floor(Math.random()*numImages);
-		if(n==current){
-			while(n==current){
-				n = Math.floor(Math.random()*numImages);
+	function getRandomNext() {
+		var n = Math.floor(Math.random() * numImages);
+		if (n == current) {
+			while (n == current) {
+				n = Math.floor(Math.random() * numImages);
 			}
 		}
 		return n;
 	}
 
 
-	function addEvents(){
-		$('#scroll-info a').on('click.index',function(e){
+	function addEvents() {
+		$('#scroll-info a').on('click.index', function (e) {
 			$(document).off('mousewheel.index touchmove.index');
 			stop();
 		})
 
-		$(document).on('mousewheel.index touchmove.index', function(e){
+		$(document).on('mousewheel.index touchmove.index', function (e) {
 			$(document).off('mousewheel.index touchmove.index');
 			bq.page.show($('#scroll-info a').attr('href'));
 		});
 	}
 
-	function removeEvents(){
+	function removeEvents() {
 		$('#scroll-info a').off('click.index');
 		$(document).off('mousewheel.index touchmove.index');
 	}
 
 	var init_timers = []
-	$(window).one('enter.page.bq', function(){
+	$(window).one('enter.page.bq', function () {
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$title.addClass('show');
 		}, 0);
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$images.eq(current).addClass('current');
 			start();
 		}, 800);
 
-		init_timers[init_timers.length] = setTimeout(function(){
+		init_timers[init_timers.length] = setTimeout(function () {
 			$('#scroll-info').addClass('show');
 		}, 1300);
 
 		init_timers[init_timers.length] = setTimeout(addEvents, 2000);
-	}).one('exit.page.bq', function(){
+	}).one('exit.page.bq', function () {
 		removeEvents();
 		stop();
 		for (var i = init_timers.length - 1; i >= 0; i--) {
